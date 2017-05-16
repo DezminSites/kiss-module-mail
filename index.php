bf89d7a8a7913e80d4a6dc3bcd360a861e7a9893
Dezmin <contato@dezmin.com.br>
2017-05-16 04:10:16 -0300

    config.json updated

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$app->map('/contato',function(){
+$app->map('/##url##',function(){
     global $m, $path, $local;
     ob_start();
     include('./modules/app_mail/rest/index.php');
@@ -8,6 +8,6 @@ $app->map('/contato',function(){
     ob_get_clean();
     $context = json_decode($context,true);
     $tpl = file_get_contents('./modules/app_mail/views/mail.html');
-    $context = (array_merge($local,$context,array("title" => "Contato - " . $local["name"])));
+    $context = (array_merge($local,$context,array("title" => "##title## - " . $local["name"])));
     echo $m->render($tpl,$context); 
 })->via('GET', 'POST');
--- rest/index.php
+++ rest/index.php
@@ -30,20 +30,20 @@ $app->post('(/:name)/', function ($name = "a") {
         //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
         $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp.dezmin.com.br';  // Specify main and backup SMTP servers
+        $mail->Host = '##host##';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'contato@dezmin.com.br';                 // SMTP username
-        $mail->Password = 'athlonk7';                           // SMTP password
+        $mail->Username = '##user##';                 // SMTP username
+        $mail->Password = '##pass##';                           // SMTP password
         //$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
         //$mail->Port = 587;                                    // TCP port to connect to
 
-        $mail->setFrom("mail@palpitepremiado.com", $name);
+        $mail->setFrom($email, $name);
 
-        $mail->addAddress("robyflc@gmail.com", "Robson");
+        $mail->addAddress("##toemail##", "##toname##");
 
         $mail->addReplyTo($email, $name);
 
-        $mail->Subject = "[Palpite] " . $subject;
+        $mail->Subject = $subject;
         $mail->Body = $message;
 
         if(!$mail->send()) {
